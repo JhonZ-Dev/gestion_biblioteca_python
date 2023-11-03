79d58292613905777846089d5bcc08da54bec8ab
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-02 21:25:52 -0500

    Update biblioteca.py

--- biblioteca.py
+++ biblioteca.py
@@ -1,2 +1,7 @@
 class Book:
-    
+    def __init__(self, title, author, isbn):
+        self.title = title
+        self.author = author
+        self.isbn = isbn
+        self.available = True
+        self.borrower = None
